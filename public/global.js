8af06f6b0b1aa64f665e4a610f4091ec34f36187
Bibek <bibekmanshrestha@gmail.com>
2019-06-19 14:21:39 -0400

    make game room functionality

--- public/global.js
+++ public/global.js
@@ -2,12 +2,84 @@ window.onload = function() {
     
   $(function () {
     var socket = io();
-    let user = {}
+    let user = {};
+    let opponents = {};
+
+
+    // New game room request
+    $('#new_room_form').submit(function(e) {
+      e.preventDefault();
+      console.log($('#new_room_name').val());
+      socket.emit('new room', $('#new_room_name').val());
+      // $('#new_room_name').val('');
+    });
+
+    // Game room create failed
+    socket.on('room create failed', function(msg) {
+      $('#new_room_form_error').html(msg);
+    });
+
+    // New game room created
+    socket.on('room created', function(room) {
+      console.log('Game room created: ' + room);
+      $('#new_room_form_error').html('New room created');
+      user.room = room;
+      console.log(user);
+      $('#initial_info').addClass('hidden');
+      $('#prior_username_form_message').html('Game room \'' + room + '\' created successfully!');
+      $('#username_form').removeClass('hidden');
+    });
+
+    // Joining an existing game room
+    $('#existing_room_form').submit(function(e) {
+      e.preventDefault();
+      console.log($('#existing_room_name').val());
+      socket.emit('join room', $('#existing_room_name').val());
+      // $('#existing_room_name').val('');
+    });
+
+    // Game room joining failed
+    socket.on('room join failed', function(msg) {
+      $('#existing_room_form_error').html(msg);
+    });
+
+    // Room full
+    socket.on('room full', function(msg) {
+      $('#existing_room_form_error').html(msg);
+    });
+
+    // Game room joined
+    socket.on('room joined', function(room) {
+      console.log('Game room joined: ' + room);
+      $('#existing_room_form_error').html('Room joined');
+      user.room = room;
+      console.log(user);
+      $('#initial_info').addClass('hidden');
+      $('#prior_username_form_message').html('Game room \'' + room + '\' joined successfully!');
+      $('#username_form').removeClass('hidden');
+    });
+
+    // Enter username
+    $('#username_form').submit(function(e) {
+      e.preventDefault();
+      var username = $('#username').val();
+      console.log(username);
+      socket.emit('username', username);
+      user.username = username;
+      $('#username_form').addClass('hidden');
+      $('#game_info').removeClass('hidden');
+    });
+
+    // Other user connected
+    socket.on('user connected', function(user) {
+      opponents[user.sid] = user;
+      console.log(opponents);
+    })
 
     // Emit user is ready to play
-    $('#ready_btn').click(function() {
-      socket.emit('player ready', );
-    });
+    // $('#ready_btn').click(function() {
+    //   socket.emit('player ready', );
+    // });
 
     // Emit chat message
     // $('form').submit(function(e){
@@ -20,4 +92,12 @@ window.onload = function() {
 
   });
 
+
+  window.onbeforeunload = function(e) {
+    e.preventDefault();
+    e.returnValue = 'You will lose all your progress';
+    // return 'You will lose all your progress';
+  }
+
 }
+
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var http = require('http').createServer(app); // importing http class
 var io = require('socket.io')(http); // importing socket.io library
 var express = require('express');
 
+var rooms = {};
+var users = {};
+
 // serving static files
 // ************* very important to include this line to make static files work **********
 app.use('/public', express.static(__dirname + '/public'));
@@ -15,17 +18,91 @@ app.get('/', function(req, res) {
 // socket.io logic
 // on connection from the user
 io.on('connection', function(socket) {
-  console.log('user connected');
+  sid = socket.id;
+  console.log('user connected: ' + sid);
+
+  users[sid] = {};
+  consoleLog();
 
   // when user send's a chat message
-  socket.on('chat message', function(msg) {
-    console.log('message: ' + msg)
-  })
+  // socket.on('chat message', function(msg) {
+  //   console.log('message: ' + msg)
+  // })
 
   // on disconnection by the user
   socket.on('disconnect', function() {
-    console.log('user disconnected');
+    sid = socket.id;
+    console.log('user disconnected: ' + sid);
+    if(users[sid].room) {
+      socket_room = users[sid].room;
+      rooms[socket_room] -= 1;
+      if (rooms[socket_room] < 1) {
+        delete rooms[socket_room];
+      }
+    }
+    delete users[sid];
+    consoleLog();
   });
+
+  // new game room
+  socket.on('new room', function(name) {
+    sid = socket.id;
+    console.log(name);
+    console.log(socket.id);
+    if(rooms[name]) {
+      console.log('room exists');
+      socket.emit('room create failed', 'The room name already exists!<br>Please provide a new name')
+      consoleLog();
+    } else {
+      socket.join(name);
+      // console.log(socket.adapter.rooms[name]);
+      rooms[name] = 1;
+      console.log('room created: ' + name);
+      users[sid] = {
+        sid: sid,
+        room: name
+      };
+      consoleLog();
+      socket.emit('room created', name);
+    }
+
+    // console.log(socket.handshake);
+  });
+
+  // Joining a room
+  socket.on('join room', function(name) {
+    sid = socket.id
+    console.log(name);
+    console.log(socket.id);
+    console.log(rooms[name]);
+    if(!rooms[name]) {
+      console.log('room doesn\'t exist');
+      socket.emit('room join failed', 'The room doesn\'t exist!<br>Please check with the creater of the room');
+      consoleLog();
+    }else if(rooms[name] >= 2){
+      console.log('Room is already full');
+      socket.emit('room full', 'The room is already full');
+      consoleLog();
+    } else {
+      socket.join(name);
+      rooms[name] += 1;
+      users[sid] = {
+        sid: sid,
+        room: name
+      };
+      consoleLog();
+      socket.emit('room joined', name);
+      socket.to(name).emit('user connected', users[sid]);
+    }
+  });
+
+  // Getting username from socket
+  socket.on('username', function(username) {
+    sid = socket.id;
+    users[sid].username = username;
+    consoleLog();
+    socket.to(users[sid].room).emit('user connected', users[sid]);
+  })
 
   // on player ready
   socket.on('player ready', function(){
@@ -34,7 +111,27 @@ io.on('connection', function(socket) {
 });
 
 
+// Helper functions
+function consoleLog() {
+  console.log('users:')
+  console.log(users);
+  console.log('rooms:');
+  console.log(rooms);
+};
+
+function roomUsers(room) {
+  var roomUsers = [];
+  for (var user in users) {
+    if (users[user].room == room) {
+      roomUsers.push(user);
+    }
+  }
+  return roomUsers;
+}
+
+// End of helper functions
+
 // listening to requests to the server by users
-http.listen(3000, function() {
+http.listen(process.env.PORT || 3000, function() {
   console.log('listening on *:3000');
 });
